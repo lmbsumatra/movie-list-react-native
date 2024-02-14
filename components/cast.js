3247de6bebe0b6d5fb214782a2df5fbea7193c2b
lmbsumatra <122442812+lmbsumatra@users.noreply.github.com>
2024-02-14 13:36:42 +0800

    ::moviescreen, ud

---
+++ components/cast.js
@@ -0,0 +1,45 @@
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import React from "react";
+import { ScrollView } from "react-native-gesture-handler";
+
+export default function Cast({ cast, navigation }) {
+  let personName = "Alissa Skovbye";
+  let characterName = "Tully Hart";
+  return (
+    <View className="my-6">
+      <Text className="text-white text-lg mx-4 mb-5">Top Cast</Text>
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ paddingHorizontal: 15 }}
+      >
+        {cast &&
+          cast.map((person, index) => {
+            return (
+              <TouchableOpacity 
+                key={index} 
+                className="mr-4 items-center"
+                onPress={()=>navigation.navigate('Person', person)}>
+                <View className="overflow-hidden rounded-full h-20 w-20 items-center border border-neutral-500">
+                  <Image
+                    className="rounded-wxl h-24 w-20"
+                    source={require("../assets/images/castImage1.png")}
+                  />
+                </View>
+                <Text className="text-white text-xs mt-1">
+                  {characterName.length > 10
+                    ? characterName.slice(0, 10) + "..."
+                    : characterName}
+                </Text>
+                <Text className="text-neutral-400 text-xs mt-1">
+                  {personName.lenght > 10
+                    ? personName.slice(0, 10) + "..."
+                    : personName}
+                </Text>
+              </TouchableOpacity>
+            );
+          })}
+      </ScrollView>
+    </View>
+  );
+}
--- screens/MovieScreen.js
+++ screens/MovieScreen.js
@@ -4,6 +4,7 @@ import {
   TouchableOpacity,
   Dimensions,
   Platform,
+  Image,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import { useNavigation, useRoute } from "@react-navigation/native";
@@ -15,6 +16,9 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { ChevronLeftIcon } from "react-native-heroicons/outline";
 import { HeartIcon } from "react-native-heroicons/solid";
 import { styles, theme } from "../theme";
+import { LinearGradient } from "expo-linear-gradient";
+import Cast from "../components/cast";
+import MovieList from "../components/movieList";
 
 var { width, height } = Dimensions.get("window");
 
@@ -25,6 +29,10 @@ export default function MovieScreen() {
   const { params: item } = useRoute();
   const [isFavourite, toggleFavourite] = useState(false);
   const navigation = useNavigation();
+  const [cast, setCast] = useState([1, 2, 3, 4, 5]);
+  const [similarMovies, setSimilarMovies] = useState([1, 2, 3, 4, 5]);
+
+  let movieName = "Firefly Lane";
 
   useEffect(() => {}, [item]);
 
@@ -37,7 +45,7 @@ export default function MovieScreen() {
         <View className="w-full">
           <SafeAreaView
             className={
-              " z-20 w-full flex-row justify-between items-center px-4 " +
+              "absolute z-20 w-full flex-row justify-between items-center px-4 " +
               topMargin
             }
           >
@@ -48,10 +56,63 @@ export default function MovieScreen() {
               <ChevronLeftIcon size="28" strokeWidth={2.5} color="white" />
             </TouchableOpacity>
             <TouchableOpacity onPress={() => toggleFavourite(!isFavourite)}>
-              <HeartIcon size="35" color={isFavourite ? theme.background : "white"} />
+              <HeartIcon
+                size="35"
+                color={isFavourite ? theme.background : "white"}
+              />
             </TouchableOpacity>
           </SafeAreaView>
+
+          <View>
+            <Image
+              source={require("../assets/images/moviePoster1.png")}
+              style={{ width, height: height * 0.55 }}
+            />
+            <LinearGradient
+              colors={["transparent", "rgba(23,23,23,0.6)", "rgba(23,23,23,1)"]}
+              style={{ width, height: height * 0.4 }}
+              start={{ x: 0.5, y: 0 }}
+              end={{ x: 0.5, y: 1 }}
+              className="absolute bottom-0"
+            />
+          </View>
         </View>
+        <View style={{ marginTop: -(height * 0.09) }} className="space-y-3">
+          <Text className="text-white text-center text-3xl font-bold tracking-wider">
+            {movieName}
+          </Text>
+
+          {/* Status, Release, Runtime */}
+          <Text className="text-neutral-400 font-semibold text-base text-center">
+            Release * 2020 * 170 min
+          </Text>
+
+          {/* Genres */}
+          <View className="flex-row justify-center mx-4 space-x-2">
+            <Text className="text-neutral-400 font-semibold text-base text-center">
+              Action *
+            </Text>
+            <Text className="text-neutral-400 font-semibold text-base text-center">
+              Thrill *
+            </Text>
+            <Text className="text-neutral-400 font-semibold text-base text-center">
+              Comedy
+            </Text>
+          </View>
+
+          {/* Movie Description */}
+          <Text className="text-neutral-400 mx-4 tracking-wide">
+            Firefly Lane follows the friendship and bond between two women from
+            the Seattle area, Kate Mularkey and Tallulah "Tully" Hart, from when
+            they become neighbors at age 14 on a street named Firefly Lane,
+            through the decades until their 40s, through the ups and downs of
+            their lives.
+          </Text>
+        </View>
+
+        {/* Casts */}
+        <Cast cast={cast} navigation={navigation} />
+
       </ScrollView>
     </GestureHandlerRootView>
   );
